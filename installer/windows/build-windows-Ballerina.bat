4a01e052b986069e650a2ba0ef0d3407b52679ef
chanaka <chanakas@wso2.com>
2018-03-30 17:42:02 +0530

    Refactor Artifacts

--- installer/windows/build-windows-Ballerina.bat
+++ installer/windows/build-windows-Ballerina.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set BALLERINA_VERSION=0.970.0-alpha1
-set BALZIP=../distribution/zip/ballerina/target/ballerina-%BALLERINA_VERSION%.zip
+set BALZIP=../../distribution/zip/ballerina/target/ballerina-%BALLERINA_VERSION%.zip
 set BALDIST=ballerina-%BALLERINA_VERSION%
 set BALPOS=windows
 set BALPARCH=x64
@@ -15,9 +15,7 @@ rmdir ballerina-%BALLERINA_VERSION% /s /q >nul 2>&1
 jar -xf %BALZIP%
 
 echo %BALDIST% build started at '%UTC_TIME%'
-resources\wix\heat dir %BALDIST% -nologo -gg -g1 -srd -sfrag -sreg -cg AppFiles -template fragment -dr INSTALLDIR -var var.SourceDir -out target\AppFiles.wxs
-
-resources\wix\candle -nologo -dbalVersion=%BALLERINA_VERSION% -dWixbalVersion=%BALLERINA_VERSION% -dArch=%BALPARCH% -dSourceDir=%BALDIST% -ext WixUtilExtension resources\installer.wxs
-
-resources\wix\light -nologo -dc1:high -sice:ICE60 -ext WixUIExtension -ext WixUtilExtension -loc resources/en-us.wxl AppFiles.wixobj installer.wixobj -o target\msi\%BALDIST%-%BALPOS%-%BALPARCH%.msi
+resources\wix\heat dir %BALDIST% -nologo -gg -g1 -srd -sfrag -sreg -cg AppFiles -template fragment -dr INSTALLDIR -var var.SourceDir -out AppFiles.wxs
+resources\wix\candle -nologo -dbalVersion=%BALLERINA_VERSION% -dWixbalVersion=0.0.0 -dArch=%BALPARCH% -dSourceDir=%BALDIST% -ext WixUtilExtension resources\installer.wxs AppFiles.wxs
+resources\wix\light -nologo -dcl:high -sice:ICE60 -ext WixUIExtension -ext WixUtilExtension -loc resources/en-us.wxl AppFiles.wixobj installer.wixobj -o target\msi\%BALDIST%-%BALPOS%-%BALPARCH%.msi
 
