4b8f08668e792294e6706bdabdf8658fde8711ac
Chanaka Cooray <chanakas@wso2.com>
2018-04-03 19:59:25 +0530

    Add arguments to the installer build script

--- installer/windows/build-windows-Ballerina.bat
+++ installer/windows/build-windows-Ballerina.bat
@@ -1,6 +1,36 @@
 @echo off
 
-set BALLERINA_VERSION=0.970.0-alpha1-SNAPSHOT
+:argumentLoop
+IF NOT "%1"=="" (
+    IF "%1"=="-dist" (
+        SET DIST=%2
+        SHIFT
+    )
+    IF "%1"=="-version" (
+        SET BALLERINA_VERSION=%2
+        SHIFT
+    )
+	SHIFT
+	goto argumentLoop
+) 
+
+
+IF "%DIST%"==""  (
+	echo The syntax of the command is incorrect. Missing argument dist.
+	goto EOF
+)
+
+IF NOT "%DIST%"=="all" IF NOT "%DIST%"=="BallerinaToolsWin64" IF NOT "%DIST%"=="BallerinaToolsWin586" IF NOT "%DIST%"=="BallerinaWin64" IF NOT "%DIST%"=="BallerinaWin586" (
+	echo The syntax of the command is incorrect. Possible arguments for dist - all, BallerinaToolsWin64, BallerinaToolsWin586, BallerinaWin64, BallerinaWin586.
+	echo Ex: -dist BallerinaToolsWin64
+	goto EOF
+)
+
+IF "%BALLERINA_VERSION%"==""  (
+	echo The syntax of the command is incorrect. Missing argument version.
+	goto EOF
+)
+
 set BALPOS=windows
 set WIXDIST=resources\wix
 
@@ -16,10 +46,14 @@ rmdir ballerina-%BALLERINA_VERSION% /s /q >nul 2>&1
 rmdir ballerina-tools-%BALLERINA_VERSION% /s /q >nul 2>&1
 rmdir target /s /q >nul 2>&1
 
-call :createBallerinaToolsWin64Installer
-call :createBallerinaToolsWin586Installer
-call :createBallerinaWin64Installer
-call :createBallerinaWin586Installer
+IF "%DIST%"=="all" (
+	call :createBallerinaToolsWin64Installer
+	call :createBallerinaToolsWin586Installer
+	call :createBallerinaWin64Installer
+	call :createBallerinaWin586Installer
+) ELSE (
+	call :create%DIST%Installer
+)
 
 goto EOF
 
@@ -57,12 +91,16 @@ call :createInstaller
 goto EOF
 
 :createInstaller
-jar -xf %BALZIP%
+rem jar -xf %BALZIP%
+rmdir ballerina-%BALLERINA_VERSION% /s /q >nul 2>&1
+rmdir ballerina-tools-%BALLERINA_VERSION% /s /q >nul 2>&1
+rmdir target\installer-resources /s /q >nul 2>&1
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%BALZIP%', '.'); }"
 
 echo %BALDIST% build started at '%UTC_TIME%' for %BALPOS% %BALPARCH%
-%WIXDIST%\heat dir %BALDIST% -nologo -gg -g1 -srd -sfrag -sreg -cg AppFiles -template fragment -dr INSTALLDIR -var var.SourceDir -out target\AppFiles.wxs
-%WIXDIST%\candle -nologo -dbalVersion=%BALLERINA_VERSION% -dWixbalVersion=1.0.0.0 -dArch=%INSTALLERPARCH% -dSourceDir=%BALDIST% -out target\ -ext WixUtilExtension resources\installer.wxs target\AppFiles.wxs
-%WIXDIST%\light -nologo -dcl:high -sice:ICE60 -ext WixUIExtension -ext WixUtilExtension -loc resources\en-us.wxl target\AppFiles.wixobj target\installer.wixobj -o target\msi\%BALDIST%-%BALPOS%-%BALPARCH%.msi
+%WIXDIST%\heat dir %BALDIST% -nologo -gg -g1 -srd -sfrag -sreg -cg AppFiles -template fragment -dr INSTALLDIR -var var.SourceDir -out target\installer-resources\AppFiles.wxs
+%WIXDIST%\candle -nologo -dbalVersion=%BALLERINA_VERSION% -dWixbalVersion=1.0.0.0 -dArch=%INSTALLERPARCH% -dSourceDir=%BALDIST% -out target\installer-resources\ -ext WixUtilExtension resources\installer.wxs target\installer-resources\AppFiles.wxs
+%WIXDIST%\light -nologo -dcl:high -sice:ICE60 -ext WixUIExtension -ext WixUtilExtension -loc resources\en-us.wxl target\installer-resources\AppFiles.wixobj target\installer-resources\installer.wixobj -o target\msi\%BALDIST%-%BALPOS%-%BALPARCH%.msi
 echo %BALDIST% build completed at '%UTC_TIME%' for %BALPOS% %BALPARCH%
 echo.
 goto EOF
