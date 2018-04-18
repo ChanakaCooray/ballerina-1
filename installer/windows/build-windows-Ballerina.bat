8db0e158983d14eea27e830d2490492eb6d842a3
Chanaka Cooray <chanakas@wso2.com>
2018-04-18 15:43:48 +0530

    Add installer resources

--- installer/windows/build-windows-Ballerina.bat
+++ installer/windows/build-windows-Ballerina.bat
@@ -2,6 +2,7 @@
 
 set BALPOS=windows
 set WIXDIST=resources\wix
+set ICONDIST=resources\icons
 set SIGNTOOLLOC="%programfiles(x86)%\Windows Kits\10\bin\10.0.16299.0\x64\signtool.exe"
 SET CERTLOC="resources\cert\wum-digicert.pfx"
 
@@ -21,34 +22,35 @@ IF NOT "%1"=="" (
 
 
 IF "%DIST%"==""  (
-	echo The syntax of the command is incorrect. Missing argument dist.
-	goto EOF
+	set DIST=all
+	rem echo The syntax of the command is incorrect. Missing argument dist.
+	rem goto EOF
 )
 
-IF NOT "%DIST%"=="all" IF NOT "%DIST%"=="BallerinaToolsWin64" IF NOT "%DIST%"=="BallerinaToolsWin586" IF NOT "%DIST%"=="BallerinaWin64" IF NOT "%DIST%"=="BallerinaWin586" (
-	echo The syntax of the command is incorrect. Possible arguments for dist - all, BallerinaToolsWin64, BallerinaToolsWin586, BallerinaWin64, BallerinaWin586.
-	echo Ex: -dist BallerinaToolsWin64
+IF NOT "%DIST%"=="all" IF NOT "%DIST%"=="BallerinaplatformWin64" IF NOT "%DIST%"=="BallerinaplatformWin586" IF NOT "%DIST%"=="BallerinaWin64" IF NOT "%DIST%"=="BallerinaWin586" (
+	echo The syntax of the command is incorrect. Possible arguments for dist - all, BallerinaplatformWin64, BallerinaplatformWin586, BallerinaWin64, BallerinaWin586.
+	echo Ex: -dist BallerinaplatformWin64
 	goto EOF
 )
 
 IF "%BALLERINA_VERSION%"==""  (
-	echo The syntax of the command is incorrect. Missing argument version.
-	goto EOF
+	set BALLERINA_VERSION=0.970.0-alpha1-SNAPSHOT
+	rem echo The syntax of the command is incorrect. Missing argument version.
+	rem goto EOF
 )
 
 for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
 set UTC_TIME=%Year%-%Month%-%Day% %Hour%:%Minute%:%Second% UTC
 
-del msi\%BALDIST%-%BALPOS%-%BALPARCH%.msi /s /q >nul 2>&1
-rmdir ballerina-%BALLERINA_VERSION% /s /q >nul 2>&1
-rmdir ballerina-tools-%BALLERINA_VERSION% /s /q >nul 2>&1
+rmdir ballerina-runtime-windows-%BALLERINA_VERSION% /s /q >nul 2>&1
+rmdir ballerina-platform-windows-%BALLERINA_VERSION% /s /q >nul 2>&1
 rmdir target /s /q >nul 2>&1
 
 IF "%DIST%"=="all" (
-	call :createBallerinaToolsWin64Installer
-	call :createBallerinaToolsWin586Installer
+	call :createBallerinaplatformWin64Installer
+	rem call :createBallerinaplatformWin586Installer
 	call :createBallerinaWin64Installer
-	call :createBallerinaWin586Installer
+	rem call :createBallerinaWin586Installer
 ) ELSE (
 	call :create%DIST%Installer
 )
@@ -56,51 +58,56 @@ IF "%DIST%"=="all" (
 goto EOF
 
 
-:createBallerinaToolsWin64Installer
-set BALZIP=..\..\distribution\zip\ballerina-tools\target\ballerina-tools-%BALLERINA_VERSION%.zip
-set BALDIST=ballerina-tools-%BALLERINA_VERSION%
+:createBallerinaplatformWin64Installer
+set BALZIP=resources\dist\ballerina-platform-windows-%BALLERINA_VERSION%.zip
+set BALDIST=ballerina-platform-windows-%BALLERINA_VERSION%
 set BALPARCH=x64
 set INSTALLERPARCH=amd64
+set MSI=ballerina-platform-%BALPOS%-installer-%BALPARCH%-%BALLERINA_VERSION%.msi
 call :createInstaller
 goto EOF
 
-:createBallerinaToolsWin586Installer
-set BALZIP=..\..\distribution\zip\ballerina-tools\target\ballerina-tools-%BALLERINA_VERSION%.zip
-set BALDIST=ballerina-tools-%BALLERINA_VERSION%
+:createBallerinaplatformWin586Installer
+set BALZIP=resources\dist\ballerina-platform-windows-%BALLERINA_VERSION%.zip
+set BALDIST=ballerina-platform-windows-%BALLERINA_VERSION%
 set BALPARCH=i586
 set INSTALLERPARCH=386
+set MSI=ballerina-platform-%BALPOS%-installer-%BALPARCH%-%BALLERINA_VERSION%.msi
 call :createInstaller
 goto EOF
 
 :createBallerinaWin64Installer
-set BALZIP=..\..\distribution\zip\ballerina\target\ballerina-%BALLERINA_VERSION%.zip
-set BALDIST=ballerina-%BALLERINA_VERSION%
+set BALZIP=resources\dist\ballerina-runtime-windows-%BALLERINA_VERSION%.zip
+set BALDIST=ballerina-runtime-windows-%BALLERINA_VERSION%
 set BALPARCH=x64
 set INSTALLERPARCH=amd64
+set MSI=ballerina-runtime-%BALPOS%-installer-%BALPARCH%-%BALLERINA_VERSION%.msi
 call :createInstaller
 goto EOF
 
 :createBallerinaWin586Installer
-set BALZIP=..\..\distribution\zip\ballerina\target\ballerina-%BALLERINA_VERSION%.zip
-set BALDIST=ballerina-%BALLERINA_VERSION%
+set BALZIP=resources\dist\ballerina-runtime-windows-%BALLERINA_VERSION%.zip
+set BALDIST=ballerina-runtime-windows-%BALLERINA_VERSION%
 set BALPARCH=i586
 set INSTALLERPARCH=386
+set MSI=ballerina-runtime-%BALPOS%-installer-%BALPARCH%-%BALLERINA_VERSION%.msi
 call :createInstaller
 goto EOF
 
 :createInstaller
 rem jar -xf %BALZIP%
-rmdir ballerina-%BALLERINA_VERSION% /s /q >nul 2>&1
-rmdir ballerina-tools-%BALLERINA_VERSION% /s /q >nul 2>&1
+rmdir ballerina-runtime-windows-%BALLERINA_VERSION% /s /q >nul 2>&1
+rmdir ballerina-platform-windows-%BALLERINA_VERSION% /s /q >nul 2>&1
 rmdir target\installer-resources /s /q >nul 2>&1
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%BALZIP%', '.'); }"
+xcopy  %ICONDIST% %BALDIST%\icons /e /i
 
 echo %BALDIST% build started at '%UTC_TIME%' for %BALPOS% %BALPARCH%
 %WIXDIST%\heat dir %BALDIST% -nologo -gg -g1 -srd -sfrag -sreg -cg AppFiles -template fragment -dr INSTALLDIR -var var.SourceDir -out target\installer-resources\AppFiles.wxs
 %WIXDIST%\candle -nologo -dbalVersion=%BALLERINA_VERSION% -dWixbalVersion=1.0.0.0 -dArch=%INSTALLERPARCH% -dSourceDir=%BALDIST% -out target\installer-resources\ -ext WixUtilExtension resources\installer.wxs target\installer-resources\AppFiles.wxs
-%WIXDIST%\light -nologo -dcl:high -sice:ICE60 -ext WixUIExtension -ext WixUtilExtension -loc resources\en-us.wxl target\installer-resources\AppFiles.wixobj target\installer-resources\installer.wixobj -o target\msi\%BALDIST%-%BALPOS%-%BALPARCH%.msi
+%WIXDIST%\light -nologo -dcl:high -sice:ICE60 -ext WixUIExtension -ext WixUtilExtension -loc resources\en-us.wxl target\installer-resources\AppFiles.wixobj target\installer-resources\installer.wixobj -o target\msi\%MSI%
 
-%SIGNTOOLLOC% sign /f %CERTLOC% /p wuminit /t http://timestamp.verisign.com/scripts/timstamp.dll target\msi\%BALDIST%-%BALPOS%-%BALPARCH%.msi
+%SIGNTOOLLOC% sign /f %CERTLOC% /p wuminit /t http://timestamp.verisign.com/scripts/timstamp.dll target\msi\%MSI%
 echo %BALDIST% build completed at '%UTC_TIME%' for %BALPOS% %BALPARCH%
 
 echo.
